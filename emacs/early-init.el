ae9847523bbbfaacfb6f85c9aad51a68bbcfd7ba
Nathan Howell <nath@nhowell.net>
2023-02-22 22:41:27 -0800

    init/straight setup updates

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,9 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
+(defvar native-comp-deferred-compilation-deny-list nil)
 (let ((file-name-handler-alist nil))
   (setq gc-cons-threshold most-positive-fixnum)
   (setq straight-check-for-modifications '(check-on-save))
-  (defvar native-comp-deferred-compilation-deny-list)
   ;; (setq native-comp-deferred-compilation-deny-list ()
         ;; comp-deferred-compilation-deny-list ())
   ;; (setq straight-repository-branch "develop")
@@ -20,22 +20,24 @@
         (eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
   (setq straight-vc-git-auto-fast-forward nil)
+  (straight-use-package 'org)
   (setq straight-use-package-by-default t)
   (straight-use-package 'use-package)
-  (setq use-package-verbose nil
-        ;; use-package-always-defer t
-        )
+  (setq use-package-verbose nil)
   (use-package org
     :demand t
     :config
-    (setq custom-enabled-themes '()))
-  (let ((orgfile (concat user-emacs-directory "emacs.org"))
-        (elfile (concat user-emacs-directory "init.el"))
-        (earlyfile (concat user-emacs-directory "early-init.el")))
-    (when (or (not (file-exists-p elfile))
-              (file-newer-than-file-p orgfile elfile)
-              (file-newer-than-file-p orgfile earlyfile))
-      (org-babel-tangle-file orgfile)))
+    (message "%s" (org-version))
+    (setq custom-enabled-themes '())
+  
+    (let ((orgfile (concat user-emacs-directory "emacs.org"))
+          (elfile (concat user-emacs-directory "init.el"))
+          (earlyfile (concat user-emacs-directory "early-init.el")))
+      (when (or (not (file-exists-p elfile))
+                (file-newer-than-file-p orgfile elfile)
+                (file-newer-than-file-p orgfile earlyfile))
+        (org-babel-tangle-file orgfile)))
+    )
   (setq gc-cons-threshold (* 3200 1000)
         gc-cons-percentage 0.6)
   
