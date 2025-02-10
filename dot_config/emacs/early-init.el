0b248e1cb20120e078cde7f4c10a8d3faafea6cd
Nathan Howell <nath@nhowell.net>
2025-02-09 20:45:01 -0800

    initial switch from straight to elpaca
    - still a bit of a mess, but works
    - had to switch to manual tangling rather than auto on startup
    - can elpaca run from early-init? would make it possible to get the
      auto-tangle back
    - theme setup (theme installs, dbus, light/dark etc) needs cleanup
    - disabled magit-file-icons (unrelated magit compatibility issue)

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -2,36 +2,9 @@
 
 (let ((file-name-handler-alist nil))
   (setq gc-cons-threshold most-positive-fixnum)
-  (setq straight-check-for-modifications '(check-on-save))
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 6))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-  (setq straight-vc-git-auto-fast-forward nil)
-  (straight-use-package 'org)
-  (setq straight-use-package-by-default t)
-  (setq use-package-verbose nil)
-  (use-package org
-    :demand t
-    :config
-    (message "%s" (org-version))
-    (setq custom-enabled-themes '())
-  
-    (let ((orgfile (concat user-emacs-directory "emacs.org"))
-          (elfile (concat user-emacs-directory "init.el"))
-          (earlyfile (concat user-emacs-directory "early-init.el")))
-      (when (or (not (file-exists-p elfile))
-                (file-newer-than-file-p orgfile elfile)
-                (file-newer-than-file-p orgfile earlyfile))
-        (org-babel-tangle-file orgfile))))
+  (setq package-enable-at-startup nil)
+  (setq use-package-verbose nil
+        use-package-always-ensure t)
   (setq gc-cons-threshold (* 3200 1000)
         gc-cons-percentage 0.6)
   
